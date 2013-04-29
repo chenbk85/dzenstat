afd9d307934e4bb9a970b1b4d6e77e82ba7c1018
ayekat <takeya@bluewin.ch>
2013-04-30 00:23:35 +0200

    MPD example programme for conky.
    * Extended functionality of MPD example programme to put conky commands
      in a file that is intended to be executed by conky.

--- mpd.c
+++ mpd.c
@@ -1,6 +1,13 @@
+/* This is a dummy programme for connecting and retrieving information from MPD.
+ */
+
 #include <mpd/client.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#define BUFLEN 512
 
 int
 main(int argc, char **argv)
@@ -10,18 +17,22 @@ main(int argc, char **argv)
 	fd_set fds;
 	struct mpd_song *song;
 	struct mpd_status *status;
+	FILE *f;
+	char title[BUFLEN], artist[BUFLEN], album[BUFLEN];
+	enum mpd_state state;
 
 	/* connect & prepare */
 	con = mpd_connection_new(NULL, 0, 0);
 	fd = mpd_connection_get_fd(con);
 
-	while (1) {
-		mpd_send_idle_mask(con, MPD_IDLE_PLAYER);
+	/* add MPD fd to the list of file descriptors
+	 * (since this is the only fd, we can put this outside the loop) */
+	FD_ZERO(&fds);
+	FD_SET(fd, &fds);
 
-		/* add fd to fd list (since this is the only fd, we might also put this
-		 * outside the loop, but keep things consistent, won't we? */
-		FD_ZERO(&fds);
-		FD_SET(fd, &fds);
+	while (1) {
+		/* only react on state change events (playing, pause, stop, ...) */
+		mpd_send_idle_mask(con, MPD_IDLE_PLAYER);
 
 		/* wait for event */
 		s = select(FD_SETSIZE, &fds, NULL, NULL, NULL);
@@ -30,13 +41,10 @@ main(int argc, char **argv)
 			mpd_connection_free(con);
 			return EXIT_FAILURE;
 		}
-		if (s == 0) {
-			return EXIT_SUCCESS;
-		}
 
-		/* empty buffer */
+		/* empty event buffer */
 		mpd_recv_idle(con, true);
-		if (mpd_connection_get_error(con)) {
+		if (mpd_connection_get_error(con) != MPD_ERROR_SUCCESS) {
 			fprintf(stderr, "MPD error: %s\n",
 					mpd_connection_get_error_message(con));
 			mpd_connection_free(con);
@@ -52,10 +60,7 @@ main(int argc, char **argv)
 			mpd_connection_free(con);
 			return EXIT_FAILURE;
 		}
-		if (mpd_status_get_state(status) == MPD_STATE_PLAY)
-			printf("playing:\n");
-		if (mpd_status_get_state(status) == MPD_STATE_PAUSE)
-			printf("paused:\n");
+		state = mpd_status_get_state(status);
 		mpd_status_free(status);
 		if (mpd_connection_get_error(con) != MPD_ERROR_SUCCESS) {
 			fprintf(stderr, "MPD error: %s\n",
@@ -67,9 +72,9 @@ main(int argc, char **argv)
 		/* get song */
 		mpd_send_current_song(con);
 		while ((song = mpd_recv_song(con)) != NULL) {
-			printf("title: %s\n", mpd_song_get_tag(song, MPD_TAG_TITLE, 0));
-			printf("artist: %s\n", mpd_song_get_tag(song, MPD_TAG_ARTIST, 0));
-			printf("album: %s\n", mpd_song_get_tag(song, MPD_TAG_ALBUM, 0));
+			snprintf(title,BUFLEN,"%s",mpd_song_get_tag(song,MPD_TAG_TITLE,0));
+			snprintf(artist,BUFLEN,"%s",mpd_song_get_tag(song,MPD_TAG_ARTIST,0));
+			snprintf(album,BUFLEN,"%s",mpd_song_get_tag(song,MPD_TAG_ALBUM,0));
 			mpd_song_free(song);
 			if (mpd_connection_get_error(con) != MPD_ERROR_SUCCESS) {
 				fprintf(stderr, "MPD error: %s\n",
@@ -78,8 +83,24 @@ main(int argc, char **argv)
 				return EXIT_FAILURE;
 			}
 		}
-		printf("\n");
+
+		/* write to file (for conky) */
+		f = fopen("mpd_conky", "w+");
+		if (f == NULL) {
+			fprintf(stderr, "failed to open conky file: %s\n", strerror(errno));
+			return EXIT_FAILURE;
+		}
+		fprintf(f, "${color AAAA55}${font Gill Sans:weight=normal:size=18}"
+		           "%s$font$color\n", title);
+		fprintf(f, "%s\n", artist);
+		fprintf(f, "${font Gill Sans:weight=thin:size=18:slant=italic}"
+		           "%s$font\n", album);
+		fprintf(f, "${image /home/ayekat/.config/conky/img/%s.jpg -n"
+		           "-s 100x100}\n", album);
+		fclose(f);
 	}
+
+	/* never reached (MUAHAHA!) */
 	return EXIT_SUCCESS;
 }
 
